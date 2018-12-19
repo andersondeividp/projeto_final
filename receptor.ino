fcd11314f8ee76ef575fddb65b67a78b3c357efb
andersondeividp <anderson.paulino@quality.com.br>
2018-12-19 12:17:13 -0200

    Add files via upload

--- receptor.ino
+++ receptor.ino
@@ -1,40 +1,22 @@
-#include<VirtualWire.h>
-
+#include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-RF24 radio(9, 10); // CE, CSN
+RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
-
-String a;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  //vw_set_rx_pin(7);
-  //vw_setup(2000);
-  //vw_rx_start();
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();      
-  Serial.println("\nAguardando comunicacao...");  
+  radio.startListening();
+  Serial.println("Aguardando comunicação ...");
 }
-
 void loop() {
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
-    Serial.println(text);
+    if(strlen(text) > 0){
+      Serial.println(text); 
+    }
   }
-  /*
-  uint8_t message[VW_MAX_MESSAGE_LEN];    
-  uint8_t msgLength = VW_MAX_MESSAGE_LEN;
-   if (vw_get_message(message, &msgLength)){              
-      for (int i = 0; i < msgLength; i++){          
-         Serial.write(message[i]);              
-      }
-      Serial.println();
-   }
-   */
 }
-
--- sender.ino
+++ sender.ino
@@ -1,106 +1,174 @@
-#include<VirtualWire.h>
-
+#include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
 
-const int trigPin = 4;
-const int echoPin = 5;
-const int ledPinY = 2;
-const int ledPinR = 7;
-const int ledPinG = 6;
-const int dataT = 8;
+const int TRIG_PIN = 5;
+const int ECHO_PIN = 4;
+const int LED_PIN_GREEN = 6;
+const int LED_PIN_YELLOW = 2;
+const int LED_PIN_RED = 3;
 
-long duration;
-int distance;
-
-long start_time;
-long end_time;
 
 long t_base;
 long t_base_min;
 long t_max;
 long t_min;
+long v_ocupada;
+long v_identificada;
 
-char data[40];
+long prev_milles;
+long intervalo;
+int v_led_red;
 
-RF24 radio(9, 10); // CE, CSN
+RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(ledPinY, OUTPUT);
-  pinMode(ledPinR, OUTPUT);
-  pinMode(ledPinG, OUTPUT);
-  Serial.begin(9600);
-  setupRadio();
-  //vw_set_tx_pin(dataT);
-  //vw_setup(2000);
-  start_time = 0;
-  end_time = 0;
+  Serial.begin(9600);  
+  initSensor();
+  initLeds();
+  initRadio();
   t_max = 2000;
   t_min = 500;
   t_base = 0;
   t_base_min = 0;
-  char *s = "vaga 1 livre";
-  send(s);
+  v_ocupada = 0;  
+  v_identificada = 0;
+
+  //SPI.begin();  
+  mfrc522.PCD_Init();
+
+  prev_milles = 0;
+  intervalo = 500;
+  v_led_red = 0;  
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance= duration*0.034/2;
-  //Serial.print("Distance: ");
-  //Serial.println(distance);
-
+void loop() {  
+  if (mfrc522.PICC_IsNewCardPresent()){        
+    if(v_identificada == 0){
+      v_identificada = v_ocupada;
+      if(v_identificada == 1)
+        enviarMensagemVagaAutorizada();          
+    }       
+  } 
+  int distance= getDistance();
   if(distance > 10){   
     long mi = millis();
     if(t_base > 0 && mi < t_base + t_max && mi - t_base_min> t_min){
-      digitalWrite(ledPinY,HIGH);
-      digitalWrite(ledPinR,LOW);
-      digitalWrite(ledPinG,LOW);
+      ascenderLuzAmarela();
     }else{
-      digitalWrite(ledPinY,LOW);
-      digitalWrite(ledPinG,HIGH);
-      digitalWrite(ledPinR,LOW);
+      ascenderLuzVerde();
       if(t_base != 0){
-        char *s = "vaga 1 livre";
-        send(s);   
+        v_ocupada = 0;
+        v_identificada = 0;
+        enviarMensagemVagaLivre();
       }
       t_base = 0;  
       t_base_min = 0;           
     }     
   }else{  
-    digitalWrite(ledPinR,HIGH);
-    digitalWrite(ledPinG,LOW);
-    digitalWrite(ledPinY,LOW);
     t_base = millis();
+    ascenderLuzVermelha();    
     if(t_base_min == 0){
-      t_base_min = t_base;
-      char *s = "vaga 1 ocupada";
-      send(s);
+      t_base_min = t_base;      
+      if(v_ocupada == 0){
+        v_ocupada = 1;
+        enviarMensagemVagaOcupada();           
+      }      
     }  
-  }  
+  }   
 }
 
-
-void send (char *message)
-{
-  //vw_send((uint8_t *)message, strlen(message));
-  //vw_wait_tx(); // Aguarda o envio de dados
-  radio.write(&message, sizeof(message));
+void initSensor(){
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
-void setupRadio ()
-{
+int getDistance(){
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  return duration*0.034/2;
+}
+
+void initLeds(){
+  pinMode(LED_PIN_RED,OUTPUT);
+  pinMode(LED_PIN_GREEN,OUTPUT);
+  pinMode(LED_PIN_YELLOW,OUTPUT);
+}
+
+void ascenderLuzAmarela(){
+  digitalWrite(LED_PIN_YELLOW,HIGH);
+  digitalWrite(LED_PIN_RED,LOW);
+  digitalWrite(LED_PIN_GREEN,LOW);
+}
+
+void ascenderLuzVermelha(){
+  digitalWrite(LED_PIN_YELLOW,LOW);  
+  digitalWrite(LED_PIN_GREEN,LOW);
+  long m = millis(); 
+  //Serial.println(m);
+  //Serial.println(prev_milles);
+  //Serial.println(intervalo);
+  if(v_identificada == 0){    
+    if(m - prev_milles > intervalo){
+      prev_milles = m;
+      if(v_led_red == 0){        
+        v_led_red = 1;
+        digitalWrite(LED_PIN_RED,HIGH);
+      }else{        
+        v_led_red = 0;
+        digitalWrite(LED_PIN_RED,LOW);
+      }
+    }
+  }else{
+    v_led_red = 1;
+    digitalWrite(LED_PIN_RED,HIGH);
+  }     
+}
+
+void ascenderLuzVerde(){
+  digitalWrite(LED_PIN_GREEN,HIGH);
+  digitalWrite(LED_PIN_RED,LOW);
+  digitalWrite(LED_PIN_YELLOW,LOW);    
+}
+
+void enviarMensagemVagaLivre(){
+  const char text[] = "vaga 1 livre";
+  radio.write(&text, sizeof(text));  
+}
+
+void enviarMensagemVagaOcupada(){
+  Serial.println("Enviando...");
+  const char text[] = "vaga 1 ocupada";
+  radio.write(&text, sizeof(text));      
+  Serial.println("Fim");
+}
+
+void enviarMensagemVagaAutorizada(){
+  const char text[] = "vaga 1 autorizada";
+  radio.write(&text, sizeof(text));      
+}
+
+void initRadio(){
+  Serial.println("Comunicando...");
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  const char text[] = "Contectado";
+  radio.write(&text, sizeof(text));
+  Serial.println("Fim");
 }
-
 
